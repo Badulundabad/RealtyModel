863dabe24c8fd90f2844d152192c2ce643868841
Badulundabad <greendmitriy407@gmail.com>
2021-01-21 01:32:15 +0300

    Update 21.01.2021

--- Model/Operation.cs
+++ Model/Operation.cs
@@ -5,20 +5,23 @@ namespace RealtyModel.Model
     public class Operation
     {
         public Guid OperationNumber { get; set; }
-        public string IpAddress { get; set; }
+        public String IpAddress { get; set; }
+        public String Token { get; set; }
+        public String Part { get; set; }
         public OperationType OperationType { get; set; }
         public ObjectType ObjectType { get; set; }
-        public string Data { get; set; }
-        public bool IsSuccessfully { get; set; }
+        public String Data { get; set; }
+        public Boolean IsSuccessfully { get; set; }
     }
     public enum OperationType
     {
         Register = 0,
         Login = 1,
-        Add = 2,
-        Change = 3,
-        Remove = 4,
-        Update = 5
+        Logout =2,
+        Add = 3,
+        Change = 4,
+        Remove = 5,
+        Update = 6
     }
     public enum ObjectType
     {
