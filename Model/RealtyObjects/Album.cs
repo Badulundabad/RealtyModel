3c1a19cfd2303efc3d3609a693b51add64bfd516
Badulundabad <greenday407@mail.ru>
2021-05-03 12:15:45 +0300

    Add Photo class

--- Model/RealtyObjects/Album.cs
+++ Model/RealtyObjects/Album.cs
@@ -1,15 +1,48 @@
-﻿using System;
-using System.Drawing;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using RealtyModel.Model.RealtyObjects;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace RealtyModel.Model
 {
     public class Album
     {
         public Int32 Id { get; set; }
-        public String Location { get; set; }
         public Byte[] Preview { get; set; }
-        public Byte[] PhotoList { get; set; }
+        public String PhotoKeys { get; set; }
+        public String Location { get; set; }
+
+        [NotMapped]
+        public ObservableCollection<Byte[]> PhotoList { get; set; }
+        public void UpdatePhotos(IEnumerable<Photo> photos)
+        {
+            Preview = photos.First().Data;
+            PhotoList = new ObservableCollection<byte[]>();
+            PhotoKeys = "";
+            foreach (Photo photo in photos)
+            {
+                PhotoList.Add(photo.Data);
+                PhotoKeys += $"{photo.Id};";
+            }
+            PhotoKeys = PhotoKeys.TrimEnd(';');
+        }
+        public void GetPhotos(IEnumerable<Photo> photoArchive)
+        {
+            foreach (Int32 key in GetPhotoKeys())
+            {
+                Byte[] data = photoArchive.First<Photo>(p => p.Id == key)?.Data;
+                if (data != null)
+                    PhotoList.Add(data);
+            }
+        }
+        public Int32[] GetPhotoKeys()
+        {
+            List<Int32> list = new List<Int32>();
+            foreach (String key in PhotoKeys.Split(';'))
+                list.Add(Int32.Parse(key));
+            return list.ToArray();
+        }
     }
 }
---
+++ Model/RealtyObjects/Photo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtyModel.Model.RealtyObjects
+{
+    public class Photo
+    {
+        [Key]
+        public Int32 Id { get; set; }
+        public Byte[] Data { get; set; }
+    }
+}
