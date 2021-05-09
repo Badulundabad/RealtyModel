af010b939ec6c40384747fb4d12cafc91639a4b3
Badulundabad <greenday407@mail.ru>
2021-05-09 11:55:36 +0300

    Add some event changes

--- Events/RealtyEventHandlers.cs
+++ Events/RealtyEventHandlers.cs
@@ -1,13 +1,10 @@
 ﻿namespace RealtyModel.Events.Realty
 {
-    public class RealtyEventHandlers
-    {
-        public delegate void ReceivedPhotoEventHandler(object sender, ReceivedPhotoEventArgs e);
-        public delegate void ReceivedFlatEventHandler(object sender, ReceivedFlatEventArgs e);
-        public delegate void ReceivedFlatUpdateEventHandler(object sender, ReceivedFlatUpdateEventArgs e);
-        public delegate void ReceivedFlatDeletionEventHandler(object sender, ReceivedFlatDeletionEventArgs e);
-        public delegate void ReceivedHouseEventHandler(object sender, ReceivedHouseEventArgs e);
-        public delegate void ReceivedHouseUpdateEventHandler(object sender, ReceivedHouseUpdateEventArgs e);
-        public delegate void ReceivedHouseDeletionEventHandler(object sender, ReceivedHouseDeletionEventArgs e);
-    }
+    public delegate void ReceivedPhotoEventHandler(object sender, ReceivedPhotoEventArgs e);
+    public delegate void ReceivedFlatEventHandler(object sender, ReceivedFlatEventArgs e);
+    public delegate void ReceivedFlatUpdateEventHandler(object sender, ReceivedFlatUpdateEventArgs e);
+    public delegate void ReceivedFlatDeletionEventHandler(object sender, ReceivedFlatDeletionEventArgs e);
+    public delegate void ReceivedHouseEventHandler(object sender, ReceivedHouseEventArgs e);
+    public delegate void ReceivedHouseUpdateEventHandler(object sender, ReceivedHouseUpdateEventArgs e);
+    public delegate void ReceivedHouseDeletionEventHandler(object sender, ReceivedHouseDeletionEventArgs e);
 }
--- Events/UIEventHandlers.cs
+++ Events/UIEventHandlers.cs
@@ -1,9 +1,6 @@
 ﻿namespace RealtyModel.Events.UI
 {
-    public class UIEventHandlers
-    {
-        public delegate void OpeningFlatFormEventHandler(object sender, OpeningFlatFormEventArgs e);
-        public delegate void FlatCreatedEventHandler(object sender, FlatCreatedEventArgs e);
-        public delegate void FlatModifiedEventHandler(object sender, FlatModifiedEventArgs e);
-    }
+    public delegate void OpeningFlatFormEventHandler(object sender, OpeningFlatFormEventArgs e);
+    public delegate void FlatCreatedEventHandler(object sender, FlatCreatedEventArgs e);
+    public delegate void FlatModifiedEventHandler(object sender, FlatModifiedEventArgs e);
 }
